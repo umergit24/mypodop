0f52f9b63418435d0f50e23178dae96553bdb57e
umerk24 <umerabdkhan24@gmail.com>
2024-09-07 00:03:59 +0500

    add pod status, cont status and start time

--- main.go
+++ main.go
@@ -4,10 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
-	"strings"
 
-	"github.com/gorilla/mux"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
@@ -18,16 +15,6 @@ import (
 )
 
 func main() {
-	// Set up the HTTP server routes
-	r := mux.NewRouter()
-	r.HandleFunc("/", listPodsHandler)
-	r.HandleFunc("/pod/{namespace}/{name}", podYAMLHandler)
-
-	fmt.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
-}
-
-func listPodsHandler(w http.ResponseWriter, r *http.Request) {
 	// Load the Kubernetes configuration
 	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
 	configOverrides := &clientcmd.ConfigOverrides{}
@@ -35,15 +22,13 @@ func listPodsHandler(w http.ResponseWriter, r *http.Request) {
 
 	config, err := kubeconfig.ClientConfig()
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Error getting Kubernetes config: %v", err), http.StatusInternalServerError)
-		return
+		log.Fatalf("Error getting Kubernetes config: %v", err)
 	}
 
 	// Create a Dynamic Client
 	dynClient, err := dynamic.NewForConfig(config)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Error creating dynamic client: %v", err), http.StatusInternalServerError)
-		return
+		log.Fatalf("Error creating dynamic client: %v", err)
 	}
 
 	// Define the GroupVersionResource for Pods
@@ -56,76 +41,93 @@ func listPodsHandler(w http.ResponseWriter, r *http.Request) {
 	// List all Pods in all namespaces
 	podList, err := dynClient.Resource(gvr).Namespace(metav1.NamespaceAll).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Error listing pods: %v", err), http.StatusInternalServerError)
-		return
+		panic(err.Error())
 	}
 
-	// Generate HTML with the list of pods
-	var builder strings.Builder
-	builder.WriteString("<html><body>")
-	builder.WriteString("<h1>Pod List</h1><ul>")
-
-	for _, item := range podList.Items {
-		podName := item.GetName()
-		namespace := item.GetNamespace()
-		builder.WriteString(fmt.Sprintf("<li><a href=\"/pod/%s/%s\">%s (%s)</a></li>", namespace, podName, podName, namespace))
-	}
-
-	builder.WriteString("</ul></body></html>")
-
-	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte(builder.String()))
+	// Print the list of Pods with their YAML manifests
+	printResourcesWithYAML(podList, "pods", dynClient, gvr)
 }
 
-func podYAMLHandler(w http.ResponseWriter, r *http.Request) {
-	// Get the pod name and namespace from the URL
-	vars := mux.Vars(r)
-	podName := vars["name"]
-	namespace := vars["namespace"]
+func printResourcesWithYAML(list *unstructured.UnstructuredList, resourceName string, dynClient dynamic.Interface, gvr schema.GroupVersionResource) {
+	fmt.Printf("Found %d %s\n", len(list.Items), resourceName)
+	for _, item := range list.Items {
+		fmt.Printf("- %s\n", item.GetName())
 
-	// Load the Kubernetes configuration
-	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
-	configOverrides := &clientcmd.ConfigOverrides{}
-	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+		// Retrieve the full unstructured object for the pod
+		pod, err := dynClient.Resource(gvr).Namespace(item.GetNamespace()).Get(context.TODO(), item.GetName(), metav1.GetOptions{})
+		if err != nil {
+			log.Printf("Error retrieving pod %s: %v", item.GetName(), err)
+			continue
+		}
 
-	config, err := kubeconfig.ClientConfig()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error getting Kubernetes config: %v", err), http.StatusInternalServerError)
-		return
+		// Remove the managedFields section
+		unstructured.RemoveNestedField(pod.Object, "metadata", "managedFields")
+
+		// Convert the unstructured object to YAML
+		podYAML, err := yaml.Marshal(pod.Object)
+		if err != nil {
+			log.Printf("Error converting pod %s to YAML: %v", item.GetName(), err)
+			continue
+		}
+
+		// Print the YAML manifest
+		fmt.Printf("---\n%s\n---\n", string(podYAML))
+
+		// Extract and print the pod status, container statuses, and Pod IP
+		printPodAndContainerStatus(pod)
 	}
+}
 
-	// Create a Dynamic Client
-	dynClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error creating dynamic client: %v", err), http.StatusInternalServerError)
-		return
+func printPodAndContainerStatus(pod *unstructured.Unstructured) {
+	// Extract the pod status
+	podStatus, _, _ := unstructured.NestedString(pod.Object, "status", "phase")
+	fmt.Printf("Pod Status: %s\n", podStatus)
+
+	// Extract the pod IP
+	podIP, _, _ := unstructured.NestedString(pod.Object, "status", "podIP")
+	fmt.Printf("Pod IP: %s\n", podIP)
+
+	// Extract the container statuses
+	containerStatuses, found, _ := unstructured.NestedSlice(pod.Object, "status", "containerStatuses")
+	if found {
+		fmt.Println("Container Statuses:")
+		for _, container := range containerStatuses {
+			if containerMap, ok := container.(map[string]interface{}); ok {
+				name := containerMap["name"].(string)
+
+				// Map to handle different states
+				stateMapping := map[string]string{
+					"running":    "Running",
+					"waiting":    "Waiting",
+					"terminated": "Terminated",
+				}
+
+				state, startTime := getContainerState(containerMap, stateMapping)
+				if startTime != "" {
+					fmt.Printf("  - Container Name: %s, State: %s, Start Time: %s\n", name, state, startTime)
+				} else {
+					fmt.Printf("  - Container Name: %s, State: %s\n", name, state)
+				}
+			}
+		}
+	} else {
+		fmt.Println("No container statuses found.")
 	}
+}
 
-	// Define the GroupVersionResource for Pods
-	gvr := schema.GroupVersionResource{
-		Group:    "",
-		Version:  "v1",
-		Resource: "pods",
+func getContainerState(containerMap map[string]interface{}, stateMapping map[string]string) (string, string) {
+	for stateType, displayState := range stateMapping {
+		if state, found := containerMap["state"].(map[string]interface{})[stateType]; found {
+			stateDetails := state.(map[string]interface{})
+			startTime := ""
+			if time, ok := stateDetails["startedAt"].(string); ok {
+				startTime = time
+			}
+			if finishTime, ok := stateDetails["finishedAt"].(string); ok {
+				startTime = finishTime
+			}
+			return displayState, startTime
+		}
 	}
-
-	// Retrieve the pod by name and namespace
-	pod, err := dynClient.Resource(gvr).Namespace(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error retrieving pod %s: %v", podName, err), http.StatusInternalServerError)
-		return
-	}
-
-	// Remove the managedFields section
-	unstructured.RemoveNestedField(pod.Object, "metadata", "managedFields")
-
-	// Convert the unstructured object to YAML
-	podYAML, err := yaml.Marshal(pod.Object)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error converting pod %s to YAML: %v", podName, err), http.StatusInternalServerError)
-		return
-	}
-
-	// Serve the YAML as plain text
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte(podYAML))
+	return "Unknown", ""
 }
